f607450c361f6f3cd5c604a2583c327c878a82a3
Daniel Macedo <danielmsv2011@hotmail.com>
2022-07-05 13:55:33 -0300

    checkUser function

--- project.js
+++ project.js
@@ -1,10 +1,29 @@
 const nameForm = document.querySelector("#name-form");
-
-const logoutBtn = document.querySelector("#logout")
-
-const welcomeContainer = document.querySelector("#welcome")
+const welcomeContainer = document.querySelector("#welcome");
+const logoutBtn = document.querySelector("#logout");
 
 nameForm.addEventListener("submit", (e) => {
-    e.preventDefault();
-})
+  e.preventDefault();
 
+  const nameInput = document.querySelector("#name");
+
+  localStorage.setItem("name", nameInput.value); // Essa propriedade consegue pegar o valor digitado no input
+
+  nameInput.value = "";
+
+  checkUser();
+});
+
+function checkUser() {
+    const userName = document.querySelector("#name")
+
+    if(userName) {
+        nameForm.style.display = "none";
+
+        welcomeContainer.style.display = "block";
+    } else {
+        window.alert("Por favor insira seu nome, no campo abaixo!");
+    }
+
+
+}
